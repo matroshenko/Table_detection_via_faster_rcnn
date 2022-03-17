ffbfa4ca4649466ce3faa4f14aed63ec37b49c43
Mikhail Atroshchenko <m.atroshenko@gmail.com>
2022-03-17 14:03:55 +0000

    Убрал лишнее.

--- convert_pdfs_to_images.py
+++ convert_pdfs_to_images.py
@@ -2,17 +2,8 @@ import argparse
 import glob
 import os
 import pdf2image
-from PyPDF2 import PdfFileReader
 from tqdm import tqdm
 
-
-def get_pdf_file_shape(pdf_file_name):
-    with open(pdf_file_name, 'rb') as pdf_file:
-        pdf_page = PdfFileReader(pdf_file).getPage(0)
-        pdf_shape = pdf_page.mediaBox
-        pdf_height = float(pdf_shape[3]-pdf_shape[1])
-        pdf_width = float(pdf_shape[2]-pdf_shape[0])
-    return pdf_height, pdf_width
 
 def main(args):
 
@@ -25,9 +16,8 @@ def main(args):
             args.dst_root_folder, relative_pdf_file_folder)
         os.makedirs(output_images_folder, exist_ok=True)
 
-        pdf_height, pdf_width = get_pdf_file_shape(pdf_file_path)
         pdf2image.convert_from_path(
-            pdf_file_path, size=(pdf_width, pdf_height), fmt='jpg', 
+            pdf_file_path, dpi=72, fmt='jpg', 
             output_folder=output_images_folder,
             single_file=True, output_file=os.path.splitext(pdf_file_name)[0],
             paths_only=True)
